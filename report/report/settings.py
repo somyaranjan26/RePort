d35b54786c676be1c48276f7e48d9df2755b815b
Somyaranjan Rout <somyaranjanmac1@gmail.com>
2020-06-26 00:21:23 +0530

    Fixed Bugs

--- report/report/settings.py
+++ report/report/settings.py
@@ -1,5 +1,5 @@
 import os
-import django_on_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'report'
 ]
 
 MIDDLEWARE = [
@@ -112,4 +113,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
