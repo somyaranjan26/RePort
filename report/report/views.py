b9e241301196c12a6c0c1068a946d1ff5612a8d1
Somyaranjan Rout <45004977+somyaranjan26@users.noreply.github.com>
2020-07-03 15:49:25 +0530

    Update views.py

--- report/report/views.py
+++ report/report/views.py
@@ -7,7 +7,16 @@ import time
 import pytz
 
 config = {
-    # paste your firebase credentials here.
+    var firebaseConfig = {
+    apiKey: "api-key",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id",
+};.
 }
 
 firebase = pyrebase.initialize_app(config);    # configuring firebase using pyrebase
