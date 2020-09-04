af9d6a32c947dc245c5931f838d46cb3fd70561c
Somyaranjan Rout <45004977+somyaranjan26@users.noreply.github.com>
2020-09-05 03:05:07 +0530

    Update settings.py

--- report/report/settings.py
+++ report/report/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = # 'Paste Your Django SECRET_KEY here'
+SECRET_KEY = 'Paste Your Django SECRET_KEY here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
